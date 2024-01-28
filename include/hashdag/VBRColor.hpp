ad7875ca10a430332873376c3994401d1e510b9b
AdamYuan <y13916619121@126.com>
2024-01-29 00:30:34 +0800

    Fast Aligned-Copy for VBRBitset

--- include/hashdag/VBRColor.hpp
+++ include/hashdag/VBRColor.hpp
@@ -65,10 +65,14 @@ private:
 	std::vector<Word> m_bits;
 	std::size_t m_bit_count = 0;
 
+	// unit bits = 1, 2, 3, 4 is guaranteed for VBR
+
 public:
 	inline const std::vector<Word> &GetData() const { return m_bits; }
 	inline void Push(Word word, Word bits, std::size_t count = 1) {
-		// bits = 1,2,3,4 is guaranteed for VBR
+		if (count == 0)
+			return;
+
 		Word bit_offset = m_bit_count & kWordMask;
 		if (bit_offset == 0)
 			m_bits.emplace_back();
@@ -97,35 +101,65 @@ public:
 			remain_bits -= kWordBits - bit_offset;
 			last_bits = remain_bits;
 
-			std::size_t new_word_count = (remain_bits >> kWordMaskBits) + ((remain_bits & kWordMask) ? 1 : 0);
+			std::size_t word_count = (remain_bits >> kWordMaskBits) + ((remain_bits & kWordMask) ? 1 : 0);
 			int r_rot = (kWordBits - bit_offset) % bits;
 			if (bits == 3) {
-				static_assert(kWordBits % 3 != 0);
-
 				Word rotr_full_words[3] = {
 				    get_rotr_full_word(0),
 				    get_rotr_full_word(1),
 				    get_rotr_full_word(2),
 				};
-				for (uint32_t i = 0; i < new_word_count; ++i) {
+				for (uint32_t i = 0; i < word_count; ++i) {
 					m_bits.push_back(rotr_full_words[r_rot]);
+					static_assert(kWordBits % 3 != 0);
 					if constexpr (kWordBits % 3 == 2)
 						r_rot = (r_rot + 2) % 3;
 					else
 						r_rot = (r_rot + 1) % 3;
 				}
 			} else
-				// bits_per_weight == 1 or 2 or 4, so that kWordBits % bits == 0 and all bits are same across words
-				m_bits.resize(m_bits.size() + new_word_count, get_rotr_full_word(r_rot));
+				// bits == 1 or 2 or 4, so that kWordBits % bits == 0 and all bits are same across words
+				m_bits.resize(m_bits.size() + word_count, get_rotr_full_word(r_rot));
 		}
-		// Mask out exceeding weight bits
+		// Mask out exceeded bits
 		if ((last_bits &= kWordMask))
 			m_bits.back() &= (Word(1) << last_bits) - Word(1);
 
 		m_bit_count += bits * count;
 	}
-	inline Word Get(std::size_t index, Word bits) {
-		// bits = 1, 2, 3, 4 is guaranteed for VBR
+	inline void Paste(const VBRBitset<Word> &src, std::size_t src_begin, std::size_t src_bits) {
+		if (src_bits == 0)
+			return;
+
+		Word bit_offset = m_bit_count & kWordMask, src_bit_offset = src_begin & kWordMask;
+		if (bit_offset == 0)
+			m_bits.emplace_back();
+
+		std::size_t remain_bits = src_bits, last_bits;
+		if (bit_offset == src_bit_offset) {
+			auto src_word_it = src.m_bits.begin() + (src_begin >> kWordMaskBits);
+			m_bits.back() |= Word(*src_word_it >> bit_offset) << bit_offset;
+
+			if (bit_offset + remain_bits <= kWordBits)
+				last_bits = remain_bits + bit_offset;
+			else {
+				remain_bits -= kWordBits - bit_offset;
+				last_bits = remain_bits;
+
+				std::size_t word_count = (remain_bits >> kWordMaskBits) + ((remain_bits & kWordMask) ? 1 : 0);
+				m_bits.insert(m_bits.end(), src_word_it + 1, src_word_it + 1 + word_count);
+			}
+		} else {
+			// TODO: Implement unaligned Paste()
+		}
+
+		// Mask out exceeded bits
+		if ((last_bits &= kWordMask))
+			m_bits.back() &= (Word(1) << last_bits) - Word(1);
+
+		m_bit_count += src_bits;
+	}
+	inline Word Get(std::size_t index, Word bits) const {
 		Word offset = index & kWordMask;
 		Word w0 = m_bits[index >> kWordMaskBits] >> offset;
 		if (offset + bits <= kWordBits) {
@@ -144,17 +178,7 @@ private:
 
 	std::vector<VBRMacroBlock> m_macro_blocks;
 	std::vector<VBRBlockHeader> m_block_headers;
-	std::vector<uint32_t> m_weight_bits;
-
-	inline uint32_t get_weight_bits(uint32_t index, uint32_t bits) const {
-		const auto get_single = [this](uint32_t index, uint32_t bits) {
-			return (m_weight_bits[index >> 5u] >> (index & 31u)) & ((1u << bits) - 1);
-		};
-		uint32_t offset = index & 31u;
-		return offset + bits > 32u
-		           ? get_single(index, 32u - offset) | (get_single(index + 1, bits + offset - 32u) << (32u - offset))
-		           : get_single(index, bits);
-	}
+	VBRBitset<uint32_t> m_weight_bits;
 
 	struct VoxelIndexInfo {
 		uint32_t macro_block_id, block_header_id, block_voxel_offset;
@@ -189,7 +213,7 @@ private:
 		uint32_t weight_index =
 		    m_macro_blocks[macro_block_id].weight_start + block.GetWeightOffset() + block_voxel_offset * weight_bits;
 		return VBRColor{R5G6B5Color(block.colors), R5G6B5Color(block.colors >> 16u),
-		                (uint8_t)get_weight_bits(weight_index, weight_bits), (uint8_t)weight_bits};
+		                (uint8_t)m_weight_bits.Get(weight_index, weight_bits), (uint8_t)weight_bits};
 	}
 
 	friend class VBRColorBlockWriter;
@@ -206,53 +230,6 @@ private:
 	VBRColorBlock *m_p_color_block;
 	uint32_t m_levels, m_weight_bit_count = 0, m_voxel_count = 0;
 
-	inline void append_weight_bits(uint32_t weight, uint32_t bits_per_weight, uint32_t weight_count) {
-		uint32_t bit_offset = m_weight_bit_count & 31u;
-		if (bit_offset == 0)
-			m_weight_bits.emplace_back();
-
-		if (weight_count == 1) {
-			m_weight_bits.back() |= weight << bit_offset;
-			if (bit_offset + bits_per_weight > 32u)
-				m_weight_bits.push_back(weight >> (bit_offset + bits_per_weight - 32u));
-
-			m_weight_bit_count += bits_per_weight;
-			return;
-		}
-
-		uint64_t weight64 = weight | (weight << bits_per_weight);
-		weight64 |= (weight64 << (bits_per_weight << 1ULL));
-		weight64 |= (weight64 << (bits_per_weight << 2ULL));
-		weight64 |= (weight64 << (bits_per_weight << 3ULL));
-		weight64 |= (weight64 << (bits_per_weight << 4ULL));
-
-		uint32_t weight_bits_remain = weight_count * bits_per_weight;
-		m_weight_bits.back() |= weight64 << bit_offset;
-		if (weight_bits_remain > 32u - bit_offset) {
-			weight_bits_remain -= 32u - bit_offset;
-
-			uint32_t new_32_count = (weight_bits_remain >> 5u) + (weight_bits_remain & 31u) ? 1 : 0;
-			uint32_t shift = (32u - bit_offset) % bits_per_weight;
-			if (bits_per_weight == 3) {
-				m_weight_bits.reserve(m_weight_bits.size() + new_32_count);
-				for (uint32_t i = 0; i < new_32_count; ++i) {
-					m_weight_bits.push_back(uint32_t(weight64 >> shift));
-					shift = (shift + 2) % 3; // (shift - 1) MOD 3. 0, 2, 1, 0, 2, 1, 0, ... sequence
-				}
-			} else {
-				// bits_per_weight == 1 or 2 or 4, so that 32 % bits_per_weight == 0
-				weight64 >>= (32u - bit_offset) % bits_per_weight;
-				// all bits are same across uint32's
-				m_weight_bits.resize(m_weight_bits.size() + new_32_count, uint32_t(weight64 >> shift));
-			}
-
-			// Mask out exceeding weight bits
-			if (weight_bits_remain & 31u)
-				m_weight_bits.back() &= (1u << (weight_bits_remain & 31u)) - 1u;
-		}
-
-		m_weight_bit_count += weight_count * bits_per_weight;
-	}
 	inline void copy_weight_bits(uint32_t src_weight_begin, uint32_t bits_per_weight, uint32_t weight_count) {
 		uint32_t bit_offset = m_weight_bit_count & 31u, src_bit_offset = src_weight_begin & 31u;
 		uint32_t weight_bits_remain = weight_count * bits_per_weight;
@@ -290,21 +267,21 @@ private:
 				    .weight_start = m_weight_bit_count,
 				});
 				// assert(voxel_index % kVoxelsPerMacroBlock == 0);
-				m_block_headers.push_back(VBRBlockHeader(color.GetColors(), 0, color.GetBitsPerWeight(), 0));
+				m_block_headers.emplace_back(color.GetColors(), 0, color.GetBitsPerWeight(), 0);
 			}
 
 			if (color.GetColors() == m_block_headers.back().colors &&
 			    color.GetBitsPerWeight() == m_block_headers.back().GetBitsPerWeight()) {
 			} else {
-				m_block_headers.push_back(VBRBlockHeader(color.GetColors(), voxel_index & (kVoxelsPerMacroBlock - 1u),
-				                                         color.GetBitsPerWeight(),
-				                                         m_weight_bit_count - m_macro_blocks.back().weight_start));
+				m_block_headers.emplace_back(color.GetColors(), voxel_index & (kVoxelsPerMacroBlock - 1u),
+				                             color.GetBitsPerWeight(),
+				                             m_weight_bit_count - m_macro_blocks.back().weight_start);
 			}
 
 			uint32_t append_voxel_count =
 			    std::min(voxel_count - i, ((macro_block_id + 1u) << kVoxelBitsPerMacroBlock) - voxel_index);
 			if (color.GetBitsPerWeight())
-				append_weight_bits(color.GetWeight(), color.GetBitsPerWeight(), append_voxel_count);
+				m_weight_bits.Push(color.GetWeight(), color.GetBitsPerWeight(), append_voxel_count);
 
 			i += append_voxel_count;
 		}
--- test/vbr_test.cpp
+++ test/vbr_test.cpp
@@ -10,12 +10,12 @@ template <typename Word> void test_push_get() {
 
 			hashdag::VBRBitset<Word> bitset;
 			bitset.Push(word, bits, 100);
-			bitset.Push(word2, bits2, 100);
+			bitset.Push(word2, bits2, 100000);
 
 			hashdag::VBRBitset<Word> bitset2;
 			for (std::size_t i = 0; i < 100; ++i)
 				bitset2.Push(word, bits);
-			for (std::size_t i = 0; i < 100; ++i)
+			for (std::size_t i = 0; i < 100000; ++i)
 				bitset2.Push(word2, bits2);
 
 			CHECK_EQ(bitset.GetData().size(), bitset2.GetData().size());
@@ -24,13 +24,25 @@ template <typename Word> void test_push_get() {
 
 			for (std::size_t i = 0; i < 100; ++i)
 				CHECK_EQ(bitset.Get(i * bits, bits), word);
-			for (std::size_t i = 0; i < 100; ++i)
+			for (std::size_t i = 0; i < 100000; ++i)
 				CHECK_EQ(bitset.Get(100 * bits + i * bits2, bits2), word2);
 
 			for (std::size_t i = 0; i < 100; ++i)
 				CHECK_EQ(bitset2.Get(i * bits, bits), word);
-			for (std::size_t i = 0; i < 100; ++i)
+			for (std::size_t i = 0; i < 100000; ++i)
 				CHECK_EQ(bitset2.Get(100 * bits + i * bits2, bits2), word2);
+
+			hashdag::VBRBitset<Word> bitset3;
+			bitset3.Push(word, bits, 99);
+			bitset3.Paste(bitset, 99 * bits, bits + 100000 * bits2);
+
+			CHECK_EQ(bitset.GetData().size(), bitset3.GetData().size());
+			for (std::size_t i = 0; i < bitset.GetData().size(); ++i)
+				CHECK_EQ(bitset.GetData()[i], bitset3.GetData()[i]);
+			for (std::size_t i = 0; i < 100; ++i)
+				CHECK_EQ(bitset3.Get(i * bits, bits), word);
+			for (std::size_t i = 0; i < 100000; ++i)
+				CHECK_EQ(bitset3.Get(100 * bits + i * bits2, bits2), word2);
 		}
 	}
 }
